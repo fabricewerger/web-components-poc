94493ced2cfcf995973f19a9d131b376b0908fb7
Fabrice Werger <fabricewerger@icloud.com>
2023-06-23 14:21:26 +0200

    added styling to web component

--- src/SpeechToText/index.js
+++ src/SpeechToText/index.js
@@ -8,6 +8,9 @@ class Speech extends HTMLElement {
         shadowRoot.appendChild(stt.cloneNode(true));
         this.final_transcript = "";
         this.recognition = undefined;
+        this.audioStream = null;
+        this.isTalking = false; // Added isTalking flag
+        this.animationRequestId = null; // Added animation request ID
     }
 
     connectedCallback() {
@@ -37,10 +40,33 @@ class Speech extends HTMLElement {
     startRecording() {
         this.final_transcript = "";
         this.recognition.start();
+        this.shadowRoot.getElementById("lvl").style.transition =
+            "background-color 0.3s"; // Add transition CSS
+        this.shadowRoot.getElementById("lvl").style.backgroundColor = "#E40046"; // Reset background color
+
+        // Check if audio stream already exists or get new audio stream
+        if (this.audioStream) {
+            this.processAudioStream();
+        } else {
+            navigator.mediaDevices
+                .getUserMedia({audio: true})
+                .then((stream) => {
+                    this.audioStream = stream;
+                    this.processAudioStream();
+                })
+                .catch((err) => {
+                    /* handle the error */
+                });
+        }
     }
 
     stopRecording() {
         this.recognition.stop();
+        cancelAnimationFrame(this.animationRequestId); // Stop animation frame updates
+        this.isTalking = false; // Reset the isTalking flag
+        this.shadowRoot.getElementById("lvl").style.backgroundColor = ""; // Reset background color
+        this.shadowRoot.getElementById("lvl").style.width = ""; // Reset width
+        this.shadowRoot.getElementById("lvl").style.height = ""; // Reset height
     }
 
     changeLanguage(event) {
@@ -48,23 +74,72 @@ class Speech extends HTMLElement {
         console.log(event);
     }
 
-    onResult(event) {
-        var interim_transcript = "";
+    processAudioStream() {
+        const audioContext = new AudioContext();
+        const analyser = audioContext.createAnalyser();
+        const microphone = audioContext.createMediaStreamSource(
+            this.audioStream
+        );
+        const javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+        analyser.smoothingTimeConstant = 0.8;
+        analyser.fftSize = 1024;
+        microphone.connect(analyser);
+        analyser.connect(javascriptNode);
+        javascriptNode.connect(audioContext.destination);
 
-        for (var i = event.resultIndex; i < event.results.length; ++i) {
+        const updateLvlSize = () => {
+            const array = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteFrequencyData(array);
+            let values = 0;
+            const length = array.length;
+            for (let i = 0; i < length; i++) {
+                values += array[i];
+            }
+            const average = values / length;
+
+            if (Math.round(average) > 15) {
+                const sizeValue = Math.round(average) + 200;
+                this.shadowRoot.getElementById("lvl").style.width =
+                    sizeValue + "px";
+                this.shadowRoot.getElementById("lvl").style.height =
+                    sizeValue + "px";
+
+                this.isTalking = true; // Person is talking
+            } else {
+                // Check if the person was talking previously
+                if (this.isTalking) {
+                    // Person has stopped talking, reset width and height
+                    this.shadowRoot.getElementById("lvl").style.width = "200px";
+                    this.shadowRoot.getElementById("lvl").style.height =
+                        "200px";
+                    this.isTalking = false; // Reset the talking flag
+                }
+            }
+
+            this.animationRequestId = requestAnimationFrame(updateLvlSize); // Continue updating
+        };
+
+        updateLvlSize();
+    }
+
+    onResult(event) {
+        let interim_transcript = "";
+
+        for (let i = event.resultIndex; i < event.results.length; ++i) {
             if (event.results[i].isFinal) {
                 this.final_transcript += event.results[i][0].transcript;
             } else {
                 interim_transcript += event.results[i][0].transcript;
             }
         }
-        console.log(event);
+
         this.shadowRoot.getElementById("result").innerText = interim_transcript;
     }
 
     onEnd() {
         this.shadowRoot.getElementById("result").innerText =
             this.final_transcript;
+        this.stopRecording();
     }
 }
 
--- src/SpeechToText/templates/Stt.ts
+++ src/SpeechToText/templates/Stt.ts
@@ -1,15 +1,176 @@
 const SttTemplate = document.createElement("template");
 
-SttTemplate.innerHTML = `<div>
- <button id="start">Start recording</button>
-        <button id="stop">Stop recording</button>
-        <select name="language" id="language">
-            <option value="nl-NL">Nedelands</option>
-            <option value="en-GB">Proper British</option>
-        </select>
+SttTemplate.innerHTML = `
+<div>
+  <div class="container">
+    <button id="start">
+      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+        <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+        <path d="M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z"/>
+      </svg>
+      <div id="lvl"></div>
+    </button>
+
+    <div class="row">
+      <div class="two-thirds">
         <p id="result"></p>
+      </div>
+      <div>
+        <div class="mic"></div>
+      </div>
+    </div>
   </div>
- 
+
+  <div class='buttons'>
+    <button id="stop">Stop recording</button>
+    <select name="language" id="language">
+      <option value="nl-NL">Nederlands</option>
+      <option value="en-GB">Proper British</option>
+    </select>
+  </div>
+</div>
+
+<style>
+  .buttons {
+    display: flex;
+    gap: 30px;
+    position: absolute;
+    left: 50%;
+    top: 90%;
+    transform: translate(-50%, -50%);
+  }
+
+  .container {
+    width: 100%;
+    height: 100vh;
+    background: #04152D;
+    color: white;
+  }
+
+  svg {
+    fill: white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 50px;
+    width: 50px;
+  }
+
+  .row {
+    display: flex;
+    justify-content: center;
+  }
+
+  .two-thirds {
+    width: 30%;
+    position: relative;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+
+  #result {
+    font-family: 'Helvetica', 'Arial', sans-serif;
+    font-size: 30px;
+  }
+
+  #start {
+    position: absolute;
+    top: 50%;
+    left: 75%;
+    transform: translate(-50%, -50%);
+  }
+
+  #stop {
+    margin: 0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    font-family: 'Helvetica', 'Arial', sans-serif;
+    background-color: #E40046;
+    border: thin solid #E40046;
+    border-radius: 4px;
+    display: inline-block;
+    color: white;
+    line-height: 1.5em;
+    font-size: 13px;
+    padding: 7px 12px;
+  }
+
+  #lvl {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    transition: width 0.3s ease, height 0.3s ease, height 0.3s ease, background 0.3s ease;
+    background: #04152D;
+    border: 1px solid white;
+  }
+
+  button {
+    all: unset;
+    position: relative;
+    z-index: 1;
+    color: black;
+  }
+
+  select {
+    margin: 0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    font-family: 'Helvetica', 'Arial', sans-serif;
+    background-color: #E40046;
+    border: thin solid #E40046;
+    border-radius: 4px;
+    display: inline-block;
+    color: white;
+    line-height: 1.5em;
+    padding: 0.5em 3.5em 0.5em 1em;
+    background-image:
+      linear-gradient(45deg, transparent 50%, white 50%),
+      linear-gradient(135deg, white 50%, transparent 50%),
+      linear-gradient(to right, #ccc, #ccc);
+    background-position:
+      calc(100% - 20px) calc(1em + 2px),
+      calc(100% - 15px) calc(1em + 2px),
+      calc(100% - 2.5em) 0.5em;
+    background-size:
+      5px 5px,
+      5px 5px,
+      1px 1.5em;
+    background-repeat: no-repeat;
+  }
+
+  select:focus {
+    background-image:
+      linear-gradient(45deg, white 50%, transparent 50%),
+      linear-gradient(135deg, transparent 50%, white 50%),
+      linear-gradient(to right, #ccc, #ccc);
+    background-position:
+      calc(100% - 15px) 1em,
+      calc(100% - 20px) 1em,
+      calc(100% - 2.5em) 0.5em;
+    background-size:
+      5px 5px,
+      5px 5px,
+      1px 1.5em;
+    background-repeat: no-repeat;
+    border-color: white;
+    outline: 0;
+  }
+
+  .styled-select select {
+    -moz-appearance: none; /* Firefox */
+    -webkit-appearance: none; /* Safari and Chrome */
+    appearance: none;
+    -webkit-padding-right: 20px;
+  }
+</style>
 `;
 
 export default SttTemplate;
